88328d705f1fcd99cef2c52af9a89c980f6d24f5
Jeongeun Lee <leeju1009@gmail.com>
2025-04-28 13:57:52 +0900

    feat: 큐브 모서리가 표현되도록 라인 추가

--- main.js
+++ main.js
@@ -19,8 +19,18 @@ const material = new THREE.MeshBasicMaterial({ color: 0x93bfcf }); // 음영이 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+const edges = new THREE.EdgesGeometry(geometry); // 사물에서 라인 추출
+const edgeLines = new THREE.LineSegments(
+  edges,
+  new THREE.LineBasicMaterial({ color: 0x547792, linewidth: 2 }) // 라인 생성
+);
+cube.add(edgeLines); // 큐브의 자식으로 추가
+
 camera.position.z = 5; // 사물과 거리 (화면을 차지하는 크기)
 
 function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
   renderer.render(scene, camera);
 }
